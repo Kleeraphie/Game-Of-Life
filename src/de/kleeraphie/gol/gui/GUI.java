e5e615282aeb890ea43950ba02ae3d854d59bc13
Raphael Kleebaum <raphaelkleebaum@yahoo.de>
2022-01-05 00:21:03 +0100

    initial commit

---
+++ src/de/kleeraphie/gol/gui/GUI.java
@@ -0,0 +1,17 @@
+package de.kleeraphie.gol.gui;
+
+public class GUI {
+	
+	public void initialize() {
+		
+	}
+
+	public void start() {
+		
+	}
+	
+	public void stop() {
+		// sets a run boolean to false that is checked after each life cycle
+	}
+
+}
